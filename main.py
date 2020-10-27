26ad1b82d04715759cf2e0892af8fef2688f5795
m0rtal <m0rtal@mail.ru>
2020-10-27 11:39:25 +0300

    переход на TF-IDF

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import pandas as pd
 from catboost.text_processing import Tokenizer
-from gensim.models import FastText
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.corpus import stopwords
+from sklearn.cluster import KMeans
+
+# nltk.download("stopwords")
+
+stopwords = set(nltk.corpus.stopwords.words("russian"))
 
 
-def tokenizer(lst):
+def tokenizer(str):
     '''
     Принимает на вход список предложений
     Возвращает массив (список списков) очищенных данных - без пунктаации и в нижнем регистре
     '''
-    return [Tokenizer(separator_type='BySense', token_types=['Word'], languages=['russian', 'english']).tokenize(
-        sentence.lower()) for sentence in lst]
+    return Tokenizer(separator_type='BySense', token_types=['Word'], languages=['russian', 'english']).tokenize(
+        str.lower())
 
 
+print("Loading data...")
 rawdata = pd.read_csv('outA.csv', engine='python', delimiter=';', names=['Page', 'Date', 'Text'], parse_dates=['Date'])
 rawdata.drop_duplicates(['Text'], inplace=True)
 
 # Объединяем все записи для последующей токенизации и отказа от пунктуации
 rawtext = rawdata['Text'].to_list()
 
-# Разбиваем предложения на слова и удаляем пунктуацию
-tokenized_sentences = tokenizer(rawtext)
-print(tokenized_sentences)
+# Проведём векторизацию
+print("Vectorizing...")
+vectorizer = TfidfVectorizer(min_df=1, tokenizer=tokenizer, stop_words=stopwords, decode_error='ignore',
+                             ngram_range=(1, 3), norm='l2')
+# Создадим матрицу векторов
+tfidf_matrix = vectorizer.fit_transform(rawtext)
 
-# Создадим и обучим модель
-model = FastText(size=300, window=3, min_count=3, workers=4, sg=1, negative=5, min_n=2, max_n=5, word_ngrams=1)
-model.build_vocab(sentences=tokenized_sentences)
-model.train(sentences=tokenized_sentences, total_examples=len(tokenized_sentences), epochs=1000)
+# Разбиваем на кластеры
+print("Clustering...")
+classifier = KMeans(n_clusters=2, verbose=2, n_jobs=2)
+clustered = classifier.fit(tfidf_matrix)
+
+rawdata["Clusters"] = clustered.labels_
+print(rawdata[["Text", "Clusters"]])
